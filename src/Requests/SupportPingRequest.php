333c38327c6b739aaeea9525bb79cd0c2d218c03
Mateusz Kiedrowski <72256857+kiedrowski@users.noreply.github.com>
2020-12-02 15:57:52 +0100

    Code refactoring

--- src/Requests/SupportPingRequest.php
+++ src/Requests/SupportPingRequest.php
@@ -5,11 +5,13 @@ namespace NGT\NMVS\Requests;
 
 use NGT\NMVS\Contracts\Parser as ParserContract;
 use NGT\NMVS\Contracts\Request as RequestContract;
+use NGT\NMVS\Credentials;
 use NGT\NMVS\Enums\Endpoint;
 use NGT\NMVS\Parsers\SupportPingParser;
-use NGT\NMVS\Requests\Abstracts\Request;
+use NGT\NMVS\SoftwareDetails;
+use Spatie\ArrayToXml\ArrayToXml;
 
-class SupportPingRequest extends Request implements RequestContract
+class SupportPingRequest implements RequestContract
 {
     /**
      * The request input.
@@ -18,14 +20,37 @@ class SupportPingRequest extends Request implements RequestContract
      */
     protected $input;
 
+    /**
+     * The credentials instance.
+     *
+     * @var  \NGT\NMVS\Credentials
+     */
+    protected $credentials;
+
+    /**
+     * The software details instance.
+     *
+     * @var  \NGT\NMVS\SoftwareDetails
+     */
+    protected $softwareDetails;
+
     public function setInput(string $input): void
     {
         $this->input = $input;
     }
 
-    public function getUseCaseNumber(): string
+    public function setCredentials(Credentials $credentials): RequestContract
     {
-        return 'SupportPing';
+        $this->credentials = $credentials;
+
+        return $this;
+    }
+
+    public function setSoftwareDetails(SoftwareDetails $softwareDetails): RequestContract
+    {
+        $this->softwareDetails = $softwareDetails;
+
+        return $this;
     }
 
     public function getEndpoint(): string
@@ -38,17 +63,39 @@ class SupportPingRequest extends Request implements RequestContract
         return new SupportPingParser();
     }
 
-    protected function getEnvelopeBody(): array
+    protected function getEnvelope(): array
     {
         return [
-            $this->getEnvelopeRequestName() => $this->getEnvelopeBodyData(),
+            'soap:Header' => [],
+            'soap:Body'   => [
+                'urn:SupportPingRequest' => [
+                    'urn1:Input' => $this->input,
+                ],
+            ],
         ];
     }
 
-    protected function getEnvelopeBodyData(): array
+    protected function getEnvelopeRootAttributes(): array
     {
         return [
-            'urn1:Input' => $this->input,
+            'rootElementName' => 'soap:Envelope',
+            '_attributes'     => [
+                'xmlns:soap' => 'http://www.w3.org/2003/05/soap-envelope',
+                'xmlns:urn'  => 'urn:wsdltypes.nmvs.eu:v4.0',
+                'xmlns:urn1' => 'urn:types.nmvs.eu:v4.0',
+            ],
         ];
+    }
+
+    public function toXml(): string
+    {
+        $envelope = new ArrayToXml(
+            $this->getEnvelope(),
+            $this->getEnvelopeRootAttributes(),
+            true,
+            'UTF-8'
+        );
+
+        return $envelope->dropXmlDeclaration()->toXml();
     }
 }
